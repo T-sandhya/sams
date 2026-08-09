47306bc8190f2e558361c3ea0bcdab022b7ffb0d
T-sandhya <sandhya.tandayam989@gmail.com>
2026-08-09 14:05:49 +0530

    Connect frontend to live backend

--- userfrontend/src/context/StoreContext.jsx
+++ userfrontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems] = useState({})
     const [food_list,setFoodList] = useState([])
-    const url='http://localhost:4000'
+    const url='https://sams-backend-otah.onrender.com'
     const [token,setToken] = useState("")
 
     const fetchFoodList = async()=>{
